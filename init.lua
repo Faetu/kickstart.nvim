b55e0edc5e2f1b3a4f2067bcf1ba7b2b2464e49b
faetu <fatos.shala@protonmail.ch>
2025-11-10 21:34:57 +0100

    make enter select and accept autocompletion

--- init.lua
+++ init.lua
@@ -836,6 +836,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<Enter>'] = { 'select_and_accept', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -1014,3 +1015,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+require 'faetu.settings'
+require 'faetu.mapping'
+require 'faetu.plugins'
